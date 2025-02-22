4c85712bfde064af540409e02478f4cabd43a21c
dylancodeverse <ratianarivodylan@gmail.com>
2025-02-22 14:20:21 +0300

    statistics service domain layer

--- lib/clean/domain/entity/statistics/list_daily_statistic.dart
+++
@@ -1,7 +0,0 @@
-import 'package:sola/clean/domain/entity/statistics/daily_statisitc.dart';
-
-class ListDailyStatistic {
-  final List<DailyStatistic> dailyStatistics;
-
-  ListDailyStatistic({required this.dailyStatistics});
-}
