1768dfb5e158182dd6cb9b4f49f9ec58d5257c9e
dylancodeverse <ratianarivodylan@gmail.com>
2025-02-26 12:04:34 +0300

    bottomnav

--- lib/presentation/UI/widgets/bottomnav.dart
+++ lib/presentation/UI/widgets/bottomnav.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:sola/presentation/UI/config/theme.dart';
 
+
 class Bottomnav extends StatelessWidget {
   const Bottomnav({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-        backgroundColor: AppTheme.scaff,
-        selectedItemColor: AppTheme.primaryColor,
-        unselectedItemColor: const Color.fromARGB(255, 42, 42, 42),
-        unselectedLabelStyle: AppTheme.smallText,
-        selectedLabelStyle: AppTheme.smallText,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Accueil', ),
-          BottomNavigationBarItem(icon: Icon(Icons.history), label: 'Résumé'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Paramètres'),
-        ],
-      );
+      backgroundColor: AppTheme.scaff,
+      selectedItemColor: AppTheme.primaryColor,
+      unselectedItemColor: const Color.fromARGB(255, 42, 42, 42),
+      unselectedLabelStyle: AppTheme.smallText,
+      selectedLabelStyle: AppTheme.smallText,
+      currentIndex: 0, // L'élément sélectionné par défaut (peut être modifié dynamiquement)
+      onTap: (index) {
+        if (index == 0) {
+          Navigator.pushNamed(context, "/"); // Redirection vers "/"
+        }
+      },
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Accueil'),
+        BottomNavigationBarItem(icon: Icon(Icons.history), label: 'Résumé'),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Paramètres'),
+      ],
+    );
   }
-  
 }
