458cb69599eaf629b79ea6b1996b47b40870345c
dylancodeverse <ratianarivodylan@gmail.com>
2025-04-02 19:45:44 +0300

    participation count manual update

--- lib/domain/service/implementation/denormalization/manual_update/denormalize_state.dart
+++ lib/domain/service/implementation/denormalization/manual_update/denormalize_state.dart
@@ -1,4 +1,5 @@
 import 'package:sola/data/interface/bus_state_custom/bus_state_custom.dart';
+import 'package:sola/domain/service/implementation/cache/participation_notpayed_count.dart';
 import 'package:sola/domain/service/implementation/notification/notification_service.dart';
 import 'package:sola/domain/service/interface/cache/i_last_update_repo.dart';
 import 'package:sola/domain/service/interface/denormalization/i_denormalize_state.dart';
@@ -9,19 +10,20 @@ class DenormalizeState implements IDenormalizeState {
   BusStateCustom busStateCustom;
 
   LastUpdateRepository lastUpdateRepository;
+  ParticipationCountCache participationCountCache ;
 
 
-  DenormalizeState({required this.busStateCustom, required this.lastUpdateRepository});
+  DenormalizeState({required this.busStateCustom, required this.lastUpdateRepository , required this.participationCountCache});
 
   @override
   Future<void> verification()  async{
     bool needsUpdate = await lastUpdateRepository.isUpdateNeeded();
     NotificationService.initialize(); // 🔔 Initialisation des notifications
     if (needsUpdate) {
-      print("🔄 Mise à jour nécessaire !");
+      // required task
       await busStateCustom.update();
       await lastUpdateRepository.save(DateTime.now());
-
+      await participationCountCache.reInitCount();
       // 💡 Afficher une notification après mise à jour
       await NotificationService.showNotification(
         title: 'Mise à jour terminée',
